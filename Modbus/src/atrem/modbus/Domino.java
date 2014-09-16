ff0115a10634362b05873251049cb581c75add72
Patryk <Patryk@10.7.7.152>
2014-09-16 08:10:28 +0200

    init

---
+++ Modbus/src/atrem/modbus/Domino.java
@@ -0,0 +1,10 @@
+package atrem.modbus;
+
+public class Domino {
+
+	public static void main(String[] args) {
+		System.out.println("dziala?");
+
+	}
+
+}
---
+++ Modbus/src/atrem/modbus/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Patryk
+ *
+ */
+package atrem.modbus;
