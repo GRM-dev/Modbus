52ca156a594b38367258df8efff2b3c953ce25bb
Mieloch <Mieloch@10.7.7.199>
2014-09-18 13:36:23 +0200

    ddd

--- Modbus/src/atrem/modbus/Test.java
+++ Modbus/src/atrem/modbus/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		Controller controller = new Controller();
+
 		controller.addAndMakeRequest(69);
 		controller.addAndMakeRequest(169);
 
