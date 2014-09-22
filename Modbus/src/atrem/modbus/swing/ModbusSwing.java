a37418a8d2205c055d9bd3add046eed3225190ee
Lucas <llukas1994@wp.pl>
2014-09-22 14:33:16 +0200

    InternalFrames

--- Modbus/src/atrem/modbus/swing/ModbusSwing.java
+++ Modbus/src/atrem/modbus/swing/ModbusSwing.java
@@ -1,90 +1,129 @@
 package atrem.modbus.swing;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.beans.PropertyVetoException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.AbstractAction;
+import javax.swing.JDesktopPane;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
 import atrem.modbus.Domino;
 
 public class ModbusSwing extends JFrame {
-	private Domino domino;
-	private final JPanel contentPanel = new JPanel();
-
+	private final JPanel			contentPanel;
+	String[]						columnNames	= {
+			"No.", "IP Address", "Port", "Registry Number", "Registry Value"};
+	int								rows		= 100;
+	int								columns		= 5;
+	private JMenuBar				menuBar;
+	private JMenu					fileMenu;
+	private JMenu					connectionMenu;
+	private JMenu					setupMenu;
+	private JDesktopPane			desk;
+	private List<JInternalFrame>	framesList;
+	private Dimension				screenSize;
+	
 	public ModbusSwing(final Domino domino) {
-		this.domino = domino;
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		contentPanel = new JPanel();
+		setTitle("Domino");
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		framesList = new ArrayList<JInternalFrame>();
 		setVisible(true);
-		setBounds(100, 100, 450, 300);
+		setMinimumSize(new Dimension(screenSize.width / 3, screenSize.height / 3));
+		setBounds(100, 100, screenSize.width, screenSize.height);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-
-		JMenuBar menuBar = new JMenuBar();
+		contentPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		desk = new JDesktopPane();
+		desk.setMinimumSize(new Dimension());
+		contentPanel.add(desk);
+		
+		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-
-		JMenu fileMenu = new JMenu("File");
+		
+		fileMenu = new JMenu("File");
 		fileMenu.add(new AbstractAction("Exit") {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
-
-		JMenu connectionMenu = new JMenu("Connection");
+		
+		connectionMenu = new JMenu("Connection");
 		connectionMenu.add(new AbstractAction("Connect") {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
 				try {
-					ConnectionSetup dialogCS = new ConnectionSetup();
-					dialogCS.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-					dialogCS.setVisible(true);
-				} catch (Exception exception) {
+					newConnection();
+				}
+				catch (Exception exception) {
 					exception.printStackTrace();
 				}
 			}
 		});
-
-		JMenu setupMenu = new JMenu("Setup");
+		
+		setupMenu = new JMenu("Setup");
 		setupMenu.add(new AbstractAction("Read/Write Definition...") {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
 				try {
-					ReadWriteDefinition dialogRWD = new ReadWriteDefinition(
-							domino);
-					dialogRWD
-							.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					ReadWriteDefinition dialogRWD = new ReadWriteDefinition(domino);
+					dialogRWD.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 					dialogRWD.setVisible(true);
-				} catch (Exception exception) {
+				}
+				catch (Exception exception) {
 					exception.printStackTrace();
 				}
 			}
 		});
-
+		
 		menuBar.add(fileMenu);
 		menuBar.add(connectionMenu);
 		menuBar.add(setupMenu);
-
-		String[] columnNames = { "No.", "IP Address", "Port",
-				"Registry Number", "Registry Value" };
-		int rows = 100;
-		int columns = 5;
-		DataTablePanel dataTablePanel = new DataTablePanel(columnNames, rows,
-				columns);
-		add(dataTablePanel);
-
+		
 		pack();
 	}
-
+	
+	private void newConnection() {
+		ConnectionSetup dialogCS = new ConnectionSetup();
+		dialogCS.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		dialogCS.setVisible(true);
+		
+		JInternalFrame iFrame = new JInternalFrame("Created from Menu");
+		framesList.add(iFrame);
+		iFrame.setResizable(true);
+		iFrame.setClosable(true);
+		iFrame.setIconifiable(true);
+		iFrame.setLocation(10, 10);
+		iFrame.getContentPane().setLayout(new BorderLayout());
+		DataTablePanel table = new DataTablePanel(columnNames, rows, columns);
+		table.setVisible(true);
+		iFrame.getContentPane().add(table);
+		iFrame.setPreferredSize(new Dimension(300, 300));
+		iFrame.pack();
+		try {
+			iFrame.setSelected(true);
+			iFrame.setMaximizable(true);
+		}
+		catch (PropertyVetoException e) {
+			e.printStackTrace();
+		}
+		iFrame.setVisible(true);
+		desk.add(iFrame);
+	}
 }
