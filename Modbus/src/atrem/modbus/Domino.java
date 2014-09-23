ec35be9b12b727ad790f8162c7dee36bfc8663fd
Mieloch <Mieloch@10.7.7.199>
2014-09-23 14:22:18 +0200

    poprawki klasy

--- Modbus/src/atrem/modbus/Domino.java
+++ Modbus/src/atrem/modbus/Domino.java
@@ -15,7 +15,6 @@ public class Domino {
 	static private int port;
 	static private ConsoleInputService consoleInput;
 	static private ConsoleOutputService consoleOutput;
-	private Connection connection;
 	private static Controller controller;
 	ModbusSwing modbusSwing;
 	private static RequestFrameFactory requestFrameFactory = new RequestFrameFactory();
@@ -23,7 +22,7 @@ public class Domino {
 	public static void main(String[] args) {
 
 		Domino domino = new Domino();
-
+		domino.runModbusGUI();
 	}
 
 	public Controller getController() {
@@ -31,6 +30,14 @@ public class Domino {
 	}
 
 	public Domino() {
+
+		// Controller controller = new Controller();
+		// controller.startConnection("10.7.7.121", 502);
+		// controller.startNewRequestTask(1);
+
+	}
+
+	private void runModbusGUI() {
 		SwingUtilities.invokeLater(new Runnable() {
 
 			@Override
@@ -39,11 +46,6 @@ public class Domino {
 
 			}
 		});
-
-		// Controller controller = new Controller();
-		// controller.startConnection("10.7.7.121", 502);
-		// controller.startNewRequestTask(1);
-
 	}
 
 	Domino(Connection connection, Controller controller) {
