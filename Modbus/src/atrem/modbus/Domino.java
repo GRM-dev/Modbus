65c1af68fae3f39032c3a987304911b2a1a8dc99
Lucas <llukas1994@wp.pl>
2014-09-24 12:27:22 +0200

    Sound repaired Move out db out of modbus

--- Modbus/src/atrem/modbus/Domino.java
+++ Modbus/src/atrem/modbus/Domino.java
@@ -2,6 +2,8 @@ package atrem.modbus;
 
 import java.io.IOException;
 
+import javafx.embed.swing.JFXPanel;
+
 import javax.swing.SwingUtilities;
 
 import atrem.modbus.frameServices.FramePairs;
@@ -9,84 +11,83 @@ import atrem.modbus.frameServices.RequestFrameFactory;
 import atrem.modbus.swing.ModbusSwing;
 
 public class Domino {
-
-	private Controller controller;
-	ModbusSwing modbusSwing;
-
-	private static RequestFrameFactory requestFrameFactory = new RequestFrameFactory();
-
+	
+	private Controller					controller;
+	ModbusSwing							modbusSwing;
+	
+	private static RequestFrameFactory	requestFrameFactory	= new RequestFrameFactory();
+	
 	public static void main(String[] args) {
-
+		
 		Domino domino = new Domino();
-
+		
 	}
-
+	
 	public Controller getController() {
 		return controller;
 	}
-
+	
 	public Domino() {
 		SwingUtilities.invokeLater(new Runnable() {
-
+			
 			@Override
 			public void run() {
 				modbusSwing = new ModbusSwing(Domino.this);
-
+				new JFXPanel();
 			}
 		});
-
+		
 		// Controller controller = new Controller();
 		// controller.startConnection("10.7.7.121", 502);
 		// controller.startNewRequestTask(1);
-
+		
 	}
-
+	
 	Domino(Connection connection, Controller controller) {
-
+		
 		controller.setConnection(connection);
 		controller.startNewRequestTask(1);
 		SwingUtilities.invokeLater(new Runnable() {
-
+			
 			@Override
 			public void run() {
 				modbusSwing = new ModbusSwing(Domino.this);
-
+				
 			}
 		});
-
+		
 		// Controller controller = new Controller();
 		// controller.startConnection("10.7.7.121", 502);
 		// controller.startNewRequestTask(1);
-
+		
 	}
-
-	public void receiveConnectionParameters(String ip, int port)
-			throws IOException {
-
+	
+	public void receiveConnectionParameters(String ip, int port) throws IOException {
+		
 		controller = new Controller(this);
 		controller.startConnection(ip, port);
-
+		
 		requestFrameFactory = controller.getRequestFrameFactory();
-
+		
 	}
-
+	
 	public static void showRequestAndResponse(FramePairs framePairs) {
 		System.out.println(framePairs);
 	}
-
-	public void creatRequestFrameFactory(int unitIdentifier,
-			int startingAdress, int quantityOfRegisters, int functionCode) {
-
+	
+	public void creatRequestFrameFactory(int unitIdentifier, int startingAdress,
+			int quantityOfRegisters, int functionCode) {
+		
 		requestFrameFactory.setQuantityOfRegisters(quantityOfRegisters);
 		requestFrameFactory.setStartingAdress(startingAdress);
 		requestFrameFactory.setUnitIdentifier(unitIdentifier);
 		controller.startNewRequestTask(0); // TODO nie wiem o co loto pawel
 	}
-
+	
 	public ModbusSwing getModbusSwing() {
 		return modbusSwing;
 	}
-
+	
 	public void showConnectionStatus(boolean isConnected) {
 		if (isConnected)
 			modbusSwing.setStatus("CONNECTED!");
--- Modbus/src/atrem/modbus/SoundPlayer.java
+++ Modbus/src/atrem/modbus/SoundPlayer.java
@@ -1,49 +1,43 @@
 package atrem.modbus;
 
 import java.io.File;
-import java.net.URL;
+import java.net.MalformedURLException;
 
-import javax.media.Manager;
-import javax.media.MediaLocator;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 import javax.media.Player;
 
 public class SoundPlayer extends Thread {
-
-	private String desktopPath = "Sounds/";
-	private String fileName;
-	Player player;
-
+	
+	private String	desktopPath	= "Sounds/";
+	private String	fileName;
+	Player			player;
+	
 	public SoundPlayer(String fileName) {
 		this.fileName = fileName;
-		player = createPlayer();
 	}
-
+	
 	public void play() {
 		run();
 	}
-
+	
 	@Override
 	public void run() {
+		String bip = fileName;
+		Media hit = null;
 		try {
-			player.realize();
-			player.start();
-		} catch (Exception e) {
+			hit = new Media(new File(desktopPath + fileName).toURI().toURL().toString());
+		}
+		catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		MediaPlayer mediaPlayer = new MediaPlayer(hit);
+		mediaPlayer.play();
 	}
-
-	public Player createPlayer() {
-		try {
-			URL url = new File(desktopPath + fileName).toURI().toURL();
-			MediaLocator locator = new MediaLocator(url);
-			return Manager.createPlayer(locator);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
+	
 	public static void main(String[] args) {
-		new SoundPlayer("sound.mp3").play();
+		new SoundPlayer("connect_sound.mp3").play();
 	}
 }
